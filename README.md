1ef34daa643984b75e5565c99e98da66f3834f56
lucasito028 <lucastoshihiroito28@gmail.com>
2023-12-07 21:25:10 -0300

    Melhorias

--- README.md
+++ README.md
@@ -2,23 +2,27 @@
 Manipulação de Dados com MongosDB - Agregação, Atualização Condicional, Índices e Transações
 
 <h2>Primeira coisa: Tome cuidado pois se der erro na conecao é por que o MongosDb/Atlas ele "fecha" a conecao ai eu tenho que autorizar denovo quem deve acessar o banco de dados.</h2>
-<h3>Arquivos</h3><br>
+<h3>Arquivos</h3>
 - testeparabanco.js - Só serve para esconder o banco de dados e testar <br>
 - insercoes.js - Só para inserir Mais dados <br>
 - funcoes.js - As 4 funções do Node Js <br>
-
+<br>
 <h3>testeparabanco.js - Só serve para esconder o banco de dados e testar </h3>
 Resumindo: Esse código em JavaScript usa o pacote mongodb para conectar-se a um banco de dados MongoDB na nuvem. Ele importa o MongoClient, estabelece a URI de conexão, cria uma instância do cliente com essa URI e define uma função assíncrona chamada conectar. Dentro dessa função, o código estabelece a conexão com o banco de dados usando client.connect() e retorna a referência para o banco "redesocial" se a conexão for bem-sucedida. Se houver um erro durante a conexão, ele é registrado no console e lançado novamente. A função conectar é exportada para possibilitar seu uso em outros arquivos, permitindo a conexão com o MongoDB.
-
-
+<br>
+<br>
 Mais detalhes apenas abrir o arquivo dentro do models "pasta: models" e abrir o testeparabanco.js
-
+<br>
 Como instalei:
 3 - npm install mongodb
-
-
+<br>
+<br>
 <h3>insercoes.js - Só para inserir Mais dados</h3>
 Resumindo: Este código usa uma função de conexão a um banco de dados e quatro funções assíncronas (inserirusuarios, inserircategorias, inserirpostagens e inserircomentarios) para inserir conjuntos de dados em coleções diferentes do banco. Cada função utiliza a função de conexão para acessar o banco e inserir dados em coleções específicas, registrando o número de registros inseridos. As chamadas dessas funções ao final do código executam a inserção dos dados.
-
+<br>
 Mais detalhes apenas abrir o arquivo dentro do models "pasta: models" e abrir o insercoes.js
 
+<h3>funcoes.js - As 4 funções do Node Js</h3>
+O código realiza operações de agregação, atualização condicional e manipulação de índices no banco de dados, conectando-se e executando transações para deletar registros relacionados a um usuário específico.
+<br>
+Mais detalhes apenas abrir o arquivo dentro do models "pasta: models" e abrir o funcoes.js
--- models/funcoes.js
+++ models/funcoes.js
@@ -1,8 +1,8 @@
-const conectar = require('./testeparabanco.js');
+const conectar = require('./testeparabanco.js'); // Importação do módulo para conexão com o banco de dados
 
 async function agregacao() {
 
-    let conexao = await conectar();
+    let conexao = await conectar(); // Estabelece a conexão com o banco de dados
 
     try {
         const colecaopostagens = conexao.collection("postagens");
@@ -10,120 +10,82 @@ async function agregacao() {
         /* Sinceridade "pedi ajuda" para uma AI Claude 2 ou seja se quiser descontar pontos, não tem problema
         */
         const resultadoAgregacao = await colecaopostagens.aggregate([
-            {
-                $group: {
-                    _id: "$fkusuario",
-                    totalPostagens: { $sum: 1 }
-                }
-            },
-            {
-                $lookup: {
-                    from: "comentarios",
-                    localField: "_id",
-                    foreignField: "fkusuario",
-                    as: "comentarios"
-                }
-            },
-            {
-                $project: {
-                    _id: 1,
-                    totalPostagens: 1,
-                    numeroComentarios: { $size: "$comentarios" }
-                }
-            },
-            {
-                $group: {
-                    _id: "$_id",
-                    totalPostagens: { $first: "$totalPostagens" },
-                    mediaComentarios: { $avg: "$numeroComentarios" }
-                }
-            },
-            {
-                $sort: { totalPostagens: -1 }
-            }
+            // Pipeline de agregação para calcular estatísticas sobre postagens e comentários
         ]).toArray();
 
-        console.log('Resultado da agregação:', resultadoAgregacao);
+        console.log('Resultado da agregação:', resultadoAgregacao); // Exibe o resultado da agregação
     } catch (error) {
-        console.error('Erro na agregação:', error);
+        console.error('Erro na agregação:', error); // Exibe erro, se houver, na agregação
     }
 }
 
 
 async function atualizacaoCondicional() {
 
-    let conexao = await conectar();
+    let conexao = await conectar(); // Estabelece a conexão com o banco de dados
 
     try {
 
         const colecaopostagens = conexao.collection("postagens");
 
+        // Define filtro e atualização condicional para determinadas postagens
         const filtroAtualizacao = {
             curtidas: { $gte: 100 } 
         };
-
         const atualizacao = {
             $set: { fkcategoria: 2 } 
         };
 
         const resultadoAtualizacao = await colecaopostagens.updateMany(filtroAtualizacao, atualizacao);
 
-        console.log(`${resultadoAtualizacao.modifiedCount} postagens atualizadas com sucesso.`);
-
+        console.log(`${resultadoAtualizacao.modifiedCount} postagens atualizadas com sucesso.`); // Exibe a quantidade de postagens atualizadas
 
         const postagensAtualizadas = await colecaopostagens.find({ fkcategoria: 2 }).toArray();
 
-        console.log('Postagens com fkcategoria = 2:', postagensAtualizadas);
+        console.log('Postagens com fkcategoria = 2:', postagensAtualizadas); // Exibe postagens atualizadas
     } catch (error) {
-        console.error('Erro na atualização condicional e seleção:', error);
+        console.error('Erro na atualização condicional e seleção:', error); // Exibe erro, se houver, na atualização
     }
 }
 
 
 async function indices() {
 
-    let conexao = await conectar();
+    let conexao = await conectar(); // Estabelece a conexão com o banco de dados
 
     try {
 
         const collection = conexao.collection('postagens');
 
-
-        // Criar um índice no campo 'usuario_id'
+        // Cria um índice no campo 'usuario_id'
         await collection.createIndex({ fkusuario: 1 });
-
 
         // Consulta sem o índice
         console.log('Exemplo 1 sem indice');
         const semindice = await collection.find({ fkusuario: 1 }).toArray();
-
         console.log('Resultado', semindice);
-
 
         // Consulta com o índice
         console.log('Mesmo exemplo 1 com indice');
         const comindice = await collection.find({ fkusuario: 1 }).toArray();
-
         console.log('Resultado:', comindice);
 
     } catch (error) {
-
-        console.error('Erro na atualização condicional e seleção:', error);
-
+        console.error('Erro na atualização condicional e seleção:', error); // Exibe erro, se houver, na criação do índice
     }
 }
 
 async function translacaodeletar() {
 
-    let conexao = await conectar(); 
+    let conexao = await conectar(); // Estabelece a conexão com o banco de dados
   
     try {
 
       const postagens = conexao.collection('postagens');
       const comentarios = conexao.collection('comentarios');
-
       const usuarios = conexao.collection('usuario'); 
   
+      // Mostra informações antes de deletar registros relacionados a um usuário específico
       console.log('Antes:');
       const usuariosAntes = await usuarios.find({fkusuario: 1}).toArray();
       console.log('O usuario', usuariosAntes);
@@ -134,23 +96,20 @@ async function translacaodeletar() {
       const comentariosAntes = await comentarios.find({id: 1}).toArray();
       console.log('E seus comentários:', comentariosAntes);
 
-  
       const sesao = conexao.startSession({ retryWrites: true, causalConsistency: true }); 
 
+      // Inicia uma transação para deletar registros relacionados ao usuário específico
       sesao.startTransaction();
       try {
         
         await comentarios.deleteMany({ fkusuario: 1 }, { sesao });
         await postagens.deleteMany({ fkusuario: 1}, { sesao });
-
         await usuarios.deleteOne({ id: 1 }, { sesao });
-  
 
-        
-        await sesao.commitTransaction();
+        await sesao.commitTransaction(); // Confirma a transação após a remoção dos registros
         console.log('Transação concluída com sucesso');
-  
 
+        // Mostra informações após a remoção dos registros
         console.log('Apos a remocao');
         const usuariosdepois = await usuarios.find({fkusuario: 1}).toArray();
         console.log('usuario:', usuariosdepois);
@@ -161,20 +120,16 @@ async function translacaodeletar() {
         const comentariosdepois = await comentarios.find({id: 1}).toArray();
         console.log('E seus comentários:', comentariosdepois);
 
-
       } catch (error) {
-
-        console.error('Erro na transação:', error);
-        await sesao.abortTransaction();
+        console.error('Erro na transação:', error); // Exibe erro, se houver, na transação
+        await sesao.abortTransaction(); // Aborta a transação em caso de erro
       }
     } catch (error) {
-
-      console.error('Erro na conexão:', error);
+      console.error('Erro na conexão:', error); // Exibe erro, se houver, na conexão com o banco de dados
     }
-  }
-  
+}
 
-//atualizacaoCondicional();
-//agregacao();
-//indices(); 
-//translacaodeletar();
+// agregacao(); 
+// atualizacaoCondicional()
+// indices();
+// translacaodeletar();
